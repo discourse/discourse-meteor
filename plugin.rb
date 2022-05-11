2d4df4a705133f1f6b35f059e253ff41c1583d6e
Jarek Radosz <jradosz@gmail.com>
2022-05-11 11:44:25 +0200

    Fix linting

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
 # name: discourse-meteor
 # about: Meteor's OAuth Plugin
 # version: 0.1
 # authors: Robin Ward
+# transpile_js: true
 
-require_dependency 'auth/oauth2_authenticator.rb'
+require_dependency "auth/oauth2_authenticator.rb"
 
 class ::OmniAuth::Strategies::Oauth2Meteor < ::OmniAuth::Strategies::OAuth2
   option :name, "oauth2_meteor"
@@ -15,35 +17,37 @@ end
 
 class MeteorAuthenticator < ::Auth::OAuth2Authenticator
   def register_middleware(omniauth)
-    omniauth.provider :oauth2_meteor,
-                      :name => 'meteor',
-                      :client_id => GlobalSetting.try(:meteor_client_id),
-                      :client_secret => GlobalSetting.try(:meteor_client_secret),
-                      :provider_ignores_state => true,
-                      :client_options => {
-                        :site => 'https://accounts.meteor.com',
-                        :authorize_url => '/oauth2/authorize',
-                        :token_url => '/oauth2/token'
-                      }
+    omniauth.provider(
+      :oauth2_meteor,
+      name: "meteor",
+      client_id: GlobalSetting.try(:meteor_client_id),
+      client_secret: GlobalSetting.try(:meteor_client_secret),
+      provider_ignores_state: true,
+      client_options: {
+        site: "https://accounts.meteor.com",
+        authorize_url: "/oauth2/authorize",
+        token_url: "/oauth2/token"
+      }
+    )
   end
 
   def after_authenticate(auth)
     result = Auth::Result.new
-    token = URI.escape(auth['credentials']['token'])
-    token.gsub!(/\+/, '%2B')
+    token = URI.escape(auth["credentials"]["token"])
+    token.gsub!(/\+/, "%2B")
 
     user = JSON.parse(open("https://accounts.meteor.com/api/v1/identity", { "Authorization" => "Bearer #{token}" }).read)
 
-    result.username = user['username']
-    if user['emails'].present?
-      email = user['emails'].find {|e| e['primary'] }
+    result.username = user["username"]
+    if user["emails"].present?
+      email = user["emails"].find { |e| e["primary"] }
       if email.present?
-        result.email = email['address']
-        result.email_valid = email['verified']
+        result.email = email["address"]
+        result.email_valid = email["verified"]
       end
     end
 
-    current_info = ::PluginStore.get("meteor", "meteor_user_#{user['id']}")
+    current_info = ::PluginStore.get("meteor", "meteor_user_#{user["id"]}")
     if current_info
       result.user = User.where(id: current_info[:user_id]).first
     end
@@ -52,12 +56,12 @@ class MeteorAuthenticator < ::Auth::OAuth2Authenticator
       result.user ||= User.find_by_email(result.email)
     end
 
-    result.extra_data = { meteor_user_id: user['id'] }
+    result.extra_data = { meteor_user_id: user["id"] }
     result
   end
 
   def after_create_account(user, auth)
-    ::PluginStore.set("meteor", "meteor_user_#{auth[:extra_data][:meteor_user_id]}", {user_id: user.id })
+    ::PluginStore.set("meteor", "meteor_user_#{auth[:extra_data][:meteor_user_id]}", { user_id: user.id })
   end
 
   def enabled?
@@ -65,14 +69,14 @@ class MeteorAuthenticator < ::Auth::OAuth2Authenticator
   end
 end
 
-auth_provider title: "with Meteor",
-              authenticator: MeteorAuthenticator.new('meteor'),
-              message: "Meteor"
+auth_provider(
+  title: "with Meteor",
+  authenticator: MeteorAuthenticator.new("meteor"),
+  message: "Meteor"
+)
 
-register_css <<CSS
-
+register_css <<~CSS
   button.btn-social.meteor {
     background-color: #de4f4f;
   }
-
 CSS
